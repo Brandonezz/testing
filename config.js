053e07691bcffccc8f86b75afd17b479497aff68
Unkindness <chrissendil99@gmail.com>
2023-11-04 01:28:49 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://movies.chrissendil77.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwODg3YWFiZGNmNzNlODQ2ZmUwZDc0ZGE5NTY2MGFjZiIsInN1YiI6IjY1NDVlMmJkZmQ0ZjgwMDEwMWI1NjMyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hOmFa_O5SWtAFYhL-Qfq0pY5Nb1esUjDsmyhqfAeavA"
+};
